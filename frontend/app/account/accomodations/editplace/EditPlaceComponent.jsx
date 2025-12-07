8b52e24867b64be1ba6315d22646c16442d3e36d
mandar-saiyan87 <mdman2257@gmail.com>
2025-12-07 22:27:24 +0530

    import changes

--- frontend/app/account/accomodations/editplace/EditPlaceComponent.jsx
+++ frontend/app/account/accomodations/editplace/EditPlaceComponent.jsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 import 'react-calendar/dist/Calendar.css';
 import TimePicker from 'react-time-picker';
 import 'react-time-picker/dist/TimePicker.css';
-import 'react-clock/dist/Clock.css';
+// import 'react-clock/dist/Clock.css';
 import Amenities from '@/components/account_components/Amenities';
 import AuthMsg from '@/components/messages/AuthMsg';
 import Cookies from 'js-cookie';
